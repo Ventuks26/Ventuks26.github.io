33197ed8e87d06778065cd1a2d5e04f87442eb56
Ventuks26 <mateventura5@gmail.com>
2019-06-08 11:08:01 -0500

    Updates

---
+++ precache-manifest.4732d3a91049c14dcce12125388d6002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc378fc78719cadd19acbdd814360679",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "24d03a5a421c0f50ec61",
+    "url": "/portafolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "4a8313770783d49c138b",
+    "url": "/portafolio/static/css/main.a14a3a99.chunk.css"
+  },
+  {
+    "revision": "24d03a5a421c0f50ec61",
+    "url": "/portafolio/static/js/2.a35f9f75.chunk.js"
+  },
+  {
+    "revision": "4a8313770783d49c138b",
+    "url": "/portafolio/static/js/main.cb90046f.chunk.js"
+  },
+  {
+    "revision": "9fd29bcc1d4ba73bed23",
+    "url": "/portafolio/static/js/runtime~main.41300385.js"
+  }
+]);
